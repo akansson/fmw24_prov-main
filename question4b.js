1de869a25d4f206c75c720d8ee11786dee1a7074
akansson <ahmetakan95@gmail.com>
2024-11-26 11:49:08 +0100

    1

---
+++ question4b.js
@@ -0,0 +1,23 @@
+// 4b: Uppdatera egenskaper med Spread Operator (2p)
+// Skriv en funktion updatePriceWithDiscount som tar emot två parametrar:
+// ett produktobjekt och en rabattprocent. Funktionen ska returnera ett nytt objekt
+// där priset är uppdaterat med den givna rabattprocenten
+
+const product2 = {
+  name: "Laptop",
+  price: 15000,
+  brand: "TechBrand",
+};
+
+function updatePriceWithDiscount(product, discount) {
+  const discountedPrice = product.price * ((100 - discount) / 100);
+
+  const updatedProduct2 = { ...product, price: discountedPrice };
+  return updatedProduct2;
+}
+
+// console.log(updatePriceWithDiscount(product2, 10));
+// Output: { name: "Laptop", price: 13500, brand: "TechBrand" }
+// Uträkning: ((100 - discount)/100) * price = rabbaterat pris
+
+module.exports = updatePriceWithDiscount;
